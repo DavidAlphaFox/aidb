6654804081d5307216e79f9a84b7557f9af26b66
David.Gao <david.alpha.fox@gmail.com>
2019-01-02 15:58:58 +0800

    use maps to pass arguments

--- src/ai_redis_sup.erl
+++ src/ai_redis_sup.erl
@@ -2,14 +2,29 @@
 -behaviour(supervisor).
 
 
--export([start_link/1]).
+-export([start_link/1,start_workers/1]).
 -export([init/1]).
+
+-define(SERVER, ?MODULE).
 
 %% {pool_name,Opts}
 
 start_link(Conf) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, Conf).
 
+start_workers({Name,Opts})->
+    PoolSize = maps:get(pool_size,Opts,5),
+    PoolBoyOpts =
+        [{name,{local,Name}},{size,PoolSize},
+         {max_overflow,0},{worker_module,ai_redis_worker}
+        ],
+    PoolSepc = poolboy:child_spec(Name,PoolBoyOpts,Opts),
+    case erlang:whereis(?SERVER) of 
+        undefined ->
+            {ok,_Pid} = ai_redis_sup:start_link(), 
+            supervisor:start_child(?SERVER,PoolSepc);
+        _ -> supervisor:start_child(?SERVER,PoolSepc)
+    end.
 
 
 init(Conf) ->
@@ -18,11 +33,7 @@ init(Conf) ->
                  period => 5},
     PoolSpecs =
         lists:map(fun({Name,Opts})->
-                          PoolSize = 
-                              case proplists:get_value(pool_size,Opts) of 
-                                  undefined -> 5;
-                                  Size -> Size
-                              end,
+                          PoolSize = maps:get(pool_size,Opts,5),
                           PoolBoyOpts =
                               [{name,{local,Name}},{size,PoolSize},
                                {max_overflow,0},{worker_module,ai_redis_worker}
