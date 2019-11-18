a90579da66757df0f9353ec0973993deb2fa2348
David.Gao <david.alpha.fox@gmail.com>
2019-11-18 09:47:08 +0800

    Add PostgreSQL tools to help build fields

--- src/ai_postgres_tool.erl
+++ src/ai_postgres_tool.erl
@@ -1,6 +1,8 @@
 -module(ai_postgres_tool).
 -export([rows_to_proplists/2,rows_to_map/2]).
 -export([rows_to_proplists/3,rows_to_map/3]).
+-export([proplists_to_insert/1,proplists_to_insert/2,proplists_to_insert/3]).
+-export([proplists_to_update/1,proplists_to_update/2,proplists_to_update/3]).
 
 rows_to_proplists(Cols,Rows)-> rows_to_proplists(Cols,Rows,undefined).
 rows_to_map(Cols,Rows)-> rows_to_map(Cols,Rows,undefined).
@@ -48,3 +50,100 @@ row_to_proplists(Cols,Row,MappedCols)->
           end,
     {_Index,Map} = lists:foldl(Fun,{1,[]},Cols),
     lists:reverse(Map).
+
+
+white_list(Fields,WhiteList)->
+    lists:foldl(fun(WhiteField,Acc)->
+                        case proplists:get_value(WhiteField,Fields) of
+                            undefined -> Acc;
+                            Value -> [{WhiteField,Value}|Acc]
+                        end
+                end,[],WhiteList).
+
+reverse_mapped_cols(MappedCols)->
+    lists:foldl(fun({Key,MappedKey},Acc)->
+                        [{MappedKey,Key}|Acc]
+                end,[],MappedCols).
+
+proplists_to_insert(Fields)-> proplists_to_insert(Fields,undefined).
+proplists_to_insert(Fields,MappedCols,undefined)-> proplists_to_insert(Fields,MappedCols);
+proplists_to_insert(Fields,MappedCols,WhiteList)->
+    FilterdField = white_list(Fields,WhiteList),
+    proplists_to_insert(FilterdField,MappedCols).
+
+proplists_to_insert(Fields,undefined) ->
+    Fun = fun({FKey,FValue},{Index,AccKeys,AccPlaces,AccValues})->
+                  IndexBin = ai_string:to_string(Index),
+                  {Index + 1, [escape_field(FKey)|AccKeys], [<<"$",IndexBin/binary>>|AccPlaces],[FValue|AccValues]}
+          end,
+    {Index,Keys,Places,Values} =
+        lists:foldl(Fun,{1,[],[],[]},Fields),
+    {Index,lists:reverse(Keys),lists:reverse(Places),lists:reverse(Values)};
+
+proplists_to_insert(Fields,MappedCols) ->
+    RMappedCols = reverse_mapped_cols(MappedCols),
+    Fun = fun({FKey,FValue},{Index,AccKeys,AccPlaces,AccValues})->
+                  Key =
+                      case proplists:get_value(FKey,RMappedCols) of
+                          undefined -> FKey;
+                          Mapped -> Mapped
+                      end,
+                  IndexBin = ai_string:to_string(Index),
+                  {Index + 1, [escape_field(Key)|AccKeys], [<<"$",IndexBin/binary>>|AccPlaces],[FValue|AccValues]}
+          end,
+    {Index,Keys,Places,Values} =
+        lists:foldl(Fun,{1,[],[],[]},Fields),
+    {Index,lists:reverse(Keys),lists:reverse(Places),lists:reverse(Values)}.
+
+proplists_to_update(Fields)-> proplists_to_update(Fields,undefined).
+proplists_to_update(Fields,MappedCols,undefined)-> proplists_to_update(Fields,MappedCols);
+proplists_to_update(Fields,MappedCols,WhiteList) ->
+    FilterdField = white_list(Fields,WhiteList),
+    proplists_to_update(FilterdField,MappedCols).
+
+proplists_to_update(Fields,undefined)->
+    Fun = fun({FKey,FValue},{Index,Instr,AccValues})->
+                  IndexBin = ai_string:to_string(Index),
+                  Place = <<"$",IndexBin/binary>>,
+                  Key = escape_field(FKey),
+                  I = <<"SET "/utf8,Key/binary," = ",Place/binary>>,
+                  {Index + 1, [I|Instr],[FValue|AccValues]}
+          end,
+    {Index,Keys,Values} =
+        lists:foldl(Fun,{1,[],[]},Fields),
+    {Index,lists:reverse(Keys),lists:reverse(Values)};
+
+proplists_to_update(Fields,MappedCols)->
+    RMappedCols = reverse_mapped_cols(MappedCols),
+    Fun = fun({FKey,FValue},{Index,Instr,AccValues})->
+                  IndexBin = ai_string:to_string(Index),
+                  Key =
+                      case proplists:get_value(FKey,RMappedCols) of
+                          undefined -> escape_field(FKey);
+                          Mapped -> escape_field(Mapped)
+                      end,
+                  Place = <<"$",IndexBin/binary>>,
+                  I = <<"SET "/utf8,Key/binary," = ",Place/binary>>,
+                  {Index + 1, [I|Instr],[FValue|AccValues]}
+          end,
+    {Index,Keys,Values} =
+        lists:foldl(Fun,{1,[],[]},Fields),
+    {Index,lists:reverse(Keys),lists:reverse(Values)}.
+
+escape_field(Field) ->
+    F = ai_string:to_string(Field),
+    if
+        F == <<"*">> -> <<$\s,F/binary,$\s>>;
+        true->
+            F1 = re:replace(F,"\"","\\\"",[global,{return,binary}]),
+            <<" \"",F1/binary,"\" ">>
+    end.
+%% https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-DOLLAR-QUOTING
+%escape_value(Value)->
+%    F = ai_string:to_string(Value),
+%    case binary:match(F,<<"'">>) of
+%        nomatch ->  <<" '",F,"' ">>;
+%        _->
+%            F1 = re:replace(F,"'","\\'",[global,{return,binary}]),
+%            <<" E'",F1,"' ">>
+%    end.
