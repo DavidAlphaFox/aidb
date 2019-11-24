88f9c25dbab4aa98712d4bd3350fb833f5e97c40
David.Gao <david.alpha.fox@gmail.com>
2019-11-24 16:15:12 +0800

    Add Database Type

--- src/ai_db.erl
+++ src/ai_db.erl
@@ -2,27 +2,116 @@
 
 -export([start_pool/2]).
 
+-export([fetch/2, persist/2]).
+-export([find_one/2,find_all/1,find_by/4,find_by/5]).
+-export([delete_all/1,delete/2,delete_by/2]).
+-export([count/1,count_by/2]).
 
-start_pool(redis,Args)->
+start_pool(redis, Args) ->
     PoolSpec = redis(Args),
-    supervisor:start_child(ai_redis_pool_sup,PoolSpec);
-start_pool(store,Args) ->
+    supervisor:start_child(ai_redis_pool_sup, PoolSpec);
+start_pool(store, Args) ->
     PoolSpec = store(Args),
-    supervisor:start_child(ai_store_pool_sup,PoolSpec).
+    supervisor:start_child(ai_store_pool_sup, PoolSpec).
 
-redis({Name,Opts})->
-    PoolSize = maps:get(pool_size,Opts,5),
-    MaxOverflow = maps:get(max_overflow,Opts,0),
-    PoolBoyOpts =
-        [{name,{local,Name}},{size,PoolSize},
-         {max_overflow,MaxOverflow},{worker_module,ai_redis_worker}
-        ],
-    ai_pool:pool_spec(Name,PoolBoyOpts,Opts).
-store({Name,Opts})->
-    PoolSize = maps:get(pool_size,Opts,5),
-    MaxOverflow = maps:get(max_overflow,Opts,0),
-    PoolBoyOpts =
-        [{name,{local,Name}},{size,PoolSize},
-         {max_overflow,MaxOverflow},{worker_module,ai_db_store}
-        ],
-    ai_pool:pool_spec(Name,PoolBoyOpts,Opts).
+
+persist(ModelName, UserModel) ->
+    Model = ai_db_model:sleep(ModelName, UserModel),
+    Store = ai_db_manager:store(ModelName),
+    case ai_db_store:persist(Store, Model) of
+      {ok, NewModel} -> ai_db_model:wakeup(NewModel);
+      Error -> exit(Error)
+    end.
+
+fetch(ModelName, Id) ->
+    Store = ai_db_manager:store(ModelName),
+    case ai_db_store:fetch(Store, ModelName, Id) of
+      {ok, Model} -> ai_db_model:wakeup(Model);
+      {error, Reason} -> Reason
+    end.
+find_one(ModelName, Conditions) ->
+  case find_by(ModelName, Conditions, 1, 0) of
+    []          -> not_found;
+    [First | _] -> First
+  end.
+find_all(ModelName) ->
+  case ai_db_store:find_all(ai_db_manager:store(ModelName), ModelName) of
+    {ok, Models} -> models_wakeup(Models);
+    Error      -> exit(Error)
+  end.
+find_by(ModelName, Conditions, Limit, Offset) ->
+  Store = ai_db_manager:store(ModelName),
+  case ai_db_store:find_by(Store, ModelName, Conditions, Limit, Offset) of
+    {ok, Models} -> models_wakeup(Models);
+    Error      -> exit(Error)
+  end.
+
+find_by(ModelName, Conditions, SortFields, Limit, Offset) ->
+  NormalizedSortFields = normalize_sort_fields(SortFields),
+  Store = ai_db_manager:store(ModelName),
+  case ai_db_store:find_by(Store, ModelName, Conditions, NormalizedSortFields, Limit, Offset) of
+    {ok, Models} -> models_wakeup(Models);
+    Error      -> exit(Error)
+  end.
+
+delete_all(ModelName) ->
+  Store = ai_db_manager:store(ModelName),
+  case ai_db_store:delete_all(Store, ModelName) of
+    {ok, NumRows} -> NumRows;
+    Error -> exit(Error)
+  end.
+
+delete(ModelName, Id) ->
+  IdField = ai_db_schema:id_name(ModelName),
+  case delete_by(ModelName, [{IdField, Id}]) of
+    1 -> true;
+    0 -> false
+  end.
+
+delete_by(ModelName, Conditions) ->
+  Store = ai_db_manager:store(ModelName),
+  case ai_db_store:delete_by(Store, ModelName, Conditions) of
+    {ok, 0} -> 0;
+    {ok, NumRows} -> NumRows;
+    Error -> exit(Error)
+  end.
+
+count(ModelName) ->
+  case ai_db_store:count(ai_db_manager:store(ModelName), ModelName) of
+    {ok, Total} -> Total;
+    Error       -> exit(Error)
+  end.
+count_by(ModelName, Conditions) ->
+  case ai_db_store:count_by(ai_db_manager:store(ModelName), ModelName, Conditions) of
+    {ok, Total} -> Total;
+    Error       -> exit(Error)
+  end.
+%%%=============================================================================
+%%% Internal functions
+%%%=============================================================================
+
+redis({Name, Opts}) ->
+    PoolSize = maps:get(pool_size, Opts, 5),
+    MaxOverflow = maps:get(max_overflow, Opts, 0),
+    PoolBoyOpts = [{name, {local, Name}}, {size, PoolSize},
+		   {max_overflow, MaxOverflow},
+		   {worker_module, ai_redis_worker}],
+    ai_pool:pool_spec(Name, PoolBoyOpts, Opts).
+
+store({Name, Opts}) ->
+    PoolSize = maps:get(pool_size, Opts, 5),
+    MaxOverflow = maps:get(max_overflow, Opts, 0),
+    PoolBoyOpts = [{name, {local, Name}}, {size, PoolSize},
+		   {max_overflow, MaxOverflow},
+		   {worker_module, ai_db_store}],
+    ai_pool:pool_spec(Name, PoolBoyOpts, Opts).
+
+models_wakeup(Models) ->
+  lists:map(fun(Model) -> ai_db_model:wakeup(Model) end, Models).
+
+normalize_sort_fields(FieldName) when is_atom(FieldName) ->
+  [{FieldName, asc}];
+normalize_sort_fields({Name, Order}) ->
+  [{Name, Order}];
+normalize_sort_fields(SortFields) when is_list(SortFields) ->
+  lists:flatmap(fun normalize_sort_fields/1, SortFields).
--- src/orm/ai_db_type.erl
+++ src/orm/ai_db_type.erl
@@ -1,11 +1,86 @@
 -module(ai_db_type).
+
 -export([is_datetime/1]).
+
+-export([cast/2]).
 
 is_datetime({_, _, _} = Date) ->
     calendar:valid_date(Date);
 is_datetime({{_, _, _} = Date, {H, M, S}}) ->
-    calendar:valid_date(Date) and
-    (H >= 0 andalso H =< 23) and
-    (M >= 0 andalso M =< 59) and
-    (S >= 0 andalso S =< 59);
+    calendar:valid_date(Date) and (H >= 0 andalso H =< 23)
+      and (M >= 0 andalso M =< 59)
+      and (S >= 0 andalso S =< 59);
 is_datetime(_) -> false.
+
+cast(_, undefined) -> {ok, undefined};
+cast(Type, Data)
+  when is_binary(Data) andalso
+    (Type == integer orelse Type == float) ->
+    cast(Type, erlang:binary_to_list(Data));
+cast(float, Data) when is_integer(Data) -> {ok, Data + 0.0};
+cast(float, Data) when is_list(Data) -> cast_float(Data);
+cast(integer, Data) when is_float(Data) -> {ok, erlang:trunc(Data)};
+cast(integer, Data) when is_list(Data) -> cast_integer(Data);
+cast(Type, Data)
+  when is_list(Data), Type /= binary, Type /= custom ->
+    cast(Type, ai_string:to_string(Data));
+cast(string, Data)
+  when is_binary(Data); is_atom(Data); is_number(Data) ->
+    {ok, ai_string:to_string(Data)};
+cast(boolean, Data) when is_binary(Data) ->
+  BinData = ai_string:to_string(Data),
+  case lists:member(BinData, [<<"true">>, <<"1">>]) of
+    true -> {ok, true};
+    false ->
+	    case lists:member(BinData, [<<"false">>, <<"0">>]) of
+	      true -> {ok, false};
+	      false -> {error, {invalid, Data}}
+	    end
+  end;
+cast(date, {_, _, _} = Data) -> cast(datetime, {Data, {0, 0, 0}});
+cast(Type, Data)
+    when erlang:is_binary(Data) andalso
+	   (Type == date orelse Type == datetime) ->
+    try {ok, iso8601:parse(Data)} catch
+      _:_ -> {error, {invalid, Data}}
+    end;
+cast(Type, Data) ->
+    Fun = maps:get(Type, primitives(), fun (_) -> false end),
+    case Fun(Data) of
+      true -> {ok, Data};
+      false -> {error, {invalid, Data}}
+    end.
+
+%%%=============================================================================
+%%% Internal functions
+%%%=============================================================================
+
+%% @private
+primitives() ->
+  #{
+    string => fun erlang:is_binary/1,
+    integer => fun erlang:is_integer/1,
+    float => fun erlang:is_float/1,
+    boolean => fun erlang:is_boolean/1,
+    date => fun is_datetime/1,
+    datetime => fun is_datetime/1,
+    binary => fun erlang:is_binary/1,
+    custom => fun (_) -> true end
+  }.
+
+cast_float(Data) ->
+  case string:to_float(Data) of
+    {error, no_float} ->
+	    case cast_integer(Data) of
+	      {ok, Integer} -> {ok, Integer + 0.0};
+	      Error -> Error
+	    end;
+    {Float, _Rest} -> {ok, Float}
+  end.
+
+%% @private
+cast_integer(Data) ->
+  case string:to_integer(Data) of
+    {error, no_integer} -> {error, {invalid, Data}};
+    {Integer, _Rest} -> {ok, Integer}
+  end.
