e04a50b73db68d9a9253f81201613300ac4b6fcb
David.Gao <david.alpha.fox@gmail.com>
2019-12-11 19:52:01 +0800

    Add new tool

--- src/orm/ai_db_model.erl
+++ src/orm/ai_db_model.erl
@@ -9,24 +9,68 @@
          set_field/3
         ]).
 -export([wakeup/1,sleep/2]).
-model_name(Model) ->
-    maps:get(name, Model, undefined).
-model_module(Model) ->
-    maps:get(module, Model, undefined).
-model_fields(Model) ->
-    maps:get(fields, Model, #{}).
+-export([build_model/2,build_model/3]).
+-compile({inline,[
+                  build_field_key/2,
+                  build_fields/4,
+                  schema_attrs/1
+                 ]}).
 
 
-get_field(Name,Model) ->
-    maps:get(Name, model_fields(Model), undefined).
+build_model(Name,Input)->
+  AttrsMap = schema_attrs(Name),
+  lists:foldl(
+    fun({Key,Attrs},Acc)->
+        KeyBin = build_field_key(Key,Attrs),
+        build_fields(Key,KeyBin,Input,Acc)
+  end,#{},AttrsMap).
+
+build_model(Name,Emnu,Input)->
+  AttrsMap = schema_attrs(Name),
+  lists:foldl(
+    fun({Key,Attrs},Acc)->
+      case lists:member(Key,Emnu) of
+        true ->
+          KeyBin = build_field_key(Key,Attrs),
+          build_fields(Key,KeyBin,Input,Acc);
+        _ -> Acc
+      end
+  end,#{},AttrsMap).
+
+build_field_key(Key,Attrs) ->
+  case lists:keyfind(as,1,Attrs) of
+    false -> ai_string:to_string(Key);
+    {as,TK} -> ai_string:to_string(TK)
+  end.
+
+build_fields(Key,KeyBin,Input,Acc)->
+  case maps:get(KeyBin,Input,undefined) of
+    undefined -> Acc;
+    Value -> Acc#{Key => Value}
+  end.
+
+
+schema_attrs(Schema)->
+  lists:map(fun(F) ->
+                {
+                 ai_db_schema:field_name(F),
+                 ai_db_schema:field_attrs(F)
+                }
+            end,ai_db_schema:schema_fields(Schema)).
+
+
+model_name(Model) -> maps:get(name, Model, undefined).
+model_module(Model) -> maps:get(module, Model, undefined).
+model_fields(Model) -> maps:get(fields, Model, #{}).
+
+get_field(Name,Model) -> maps:get(Name, model_fields(Model), undefined).
 
 set_field(FieldName, FieldValue, Model = #{fields := Fields}) ->
   maps:put(fields, maps:put(FieldName, FieldValue, Fields), Model);
 set_field(FieldName, FieldValue, Fields) ->
   maps:put(FieldName, FieldValue, Fields).
 
-new_model(Name) ->
-    new_model(Name, #{}).
+new_model(Name) -> new_model(Name, #{}).
 
 new_model(Name, Fields) ->
   Module = ai_db_manager:attr_value(Name, module),
