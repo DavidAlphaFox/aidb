208d3de1e0a4199975a51cd21b9d24bf7deca131
David.Gao <david.alpha.fox@gmail.com>
2020-02-15 17:02:44 +0800

    Fix insert seq

--- src/postgres/ai_postgres_insert_builder.erl
+++ src/postgres/ai_postgres_insert_builder.erl
@@ -30,7 +30,7 @@ build_fields(#ai_db_query_context{
                } = Ctx)->
   FieldsAndValues = transform(Query#ai_db_query.fields),
   {Fields, Values} =
-        lists:foldr(
+        lists:foldl(
           fun({K, V}, {Fs, Vs}) ->
                   {[ai_postgres_escape:escape_field(K)|Fs], [V|Vs]}
           end, {[], []}, FieldsAndValues),
@@ -38,13 +38,13 @@ build_fields(#ai_db_query_context{
   SlotsFun = fun(N) -> NBin = erlang:integer_to_binary(N), <<"$",NBin/binary>>  end,
   NextSlot = Slot + erlang:length(Values),
   InsertSlots = lists:map(SlotsFun, lists:seq(Slot,NextSlot-1)),
-  FieldsCSV = ai_string:join(Fields, <<" , ">>),
+  FieldsCSV = ai_string:join(lists:reverse(Fields), <<" , ">>),
   InsertSlotsCSV = ai_string:join(InsertSlots, <<" , ">>),
 
   Ctx#ai_db_query_context{
     sql = <<Sql/binary," ( ", FieldsCSV/binary, " ) ",
             " VALUES "," ( ", InsertSlotsCSV/binary, " ) ">>,
-    bindings = Bindings ++ Values,
+    bindings = Bindings ++ lists:reverse(Values),
     slot = NextSlot
    }.
 
